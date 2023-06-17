4d588cf62ca3f84190f3545c0d23c651f39372ed
de-naissance <deniseliseev1999@mail.ru>
2023-06-17 13:30:31 +0500

    Настроен shimmerEffect

--- app/src/main/java/com/example/rickandmortyapp/ui/components/ShimmerRecipeCardItem.kt
+++ app/src/main/java/com/example/rickandmortyapp/ui/components/ShimmerRecipeCardItem.kt
@@ -1,7 +1,6 @@
 package com.example.rickandmortyapp.ui.components
 
 import androidx.compose.animation.core.animateFloat
-import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
@@ -26,8 +25,10 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.toSize
+import com.example.rickandmortyapp.ui.theme.RickAndMortyAppTheme
 
 @Composable
 fun LoadingEpisodeCard(modifier: Modifier = Modifier) {
@@ -73,9 +74,9 @@ fun Modifier.shimmerEffect(): Modifier = composed {
     background(
         brush = Brush.linearGradient(
             colors = listOf(
-                 MaterialTheme.colorScheme.background,
-                 MaterialTheme.colorScheme.error,
-                 MaterialTheme.colorScheme.secondary,
+                 MaterialTheme.colorScheme.onSurfaceVariant.copy(alpha = 0.5f),
+                 MaterialTheme.colorScheme.onSurfaceVariant,
+                 MaterialTheme.colorScheme.outline,
             ),
             start = Offset(startOffsetX, 0f),
             end = Offset(startOffsetX + size.width, size.height)
@@ -84,4 +85,63 @@ fun Modifier.shimmerEffect(): Modifier = composed {
         .onGloballyPositioned {
             size = it.size.toSize()
         }
+}
+
+@Preview(widthDp = 200)
+@Composable
+fun TestLoadingEpisode() {
+    Column(){
+        Card(
+            modifier = Modifier
+                .padding(6.dp)
+        ) {
+            Column(
+                modifier = Modifier.padding(10.dp)
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth(0.3f)
+                        .height(20.dp)
+                        .clip(MaterialTheme.shapes.small)
+                        .shimmerEffect()
+                )
+                Spacer(modifier = Modifier.height(6.dp))
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth(0.8f)
+                        .height(20.dp)
+                        .clip(MaterialTheme.shapes.small)
+                        .shimmerEffect()
+                )
+            }
+        }
+
+        RickAndMortyAppTheme(useDarkTheme = true) {
+            Card(
+                modifier = Modifier
+                    .padding(6.dp)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .padding(10.dp)
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth(0.3f)
+                            .height(20.dp)
+                            .clip(MaterialTheme.shapes.small)
+                            .shimmerEffect()
+                    )
+                    Spacer(modifier = Modifier.height(6.dp))
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth(0.8f)
+                            .height(20.dp)
+                            .clip(MaterialTheme.shapes.small)
+                            .shimmerEffect()
+                    )
+                }
+            }
+        }
+    }
 }
